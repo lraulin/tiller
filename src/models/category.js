86748c3d2e5aac430d6efd7fc5b5c2f6ac1fa81e
Lee Raulin <leeraulin@gmail.com>
2023-08-30 15:30:44 -0400

    ref: category

--- src/models/category.js
+++ src/models/category.js
@@ -1,125 +1,110 @@
 import CategoryType from "../models/category-type-enum.js";
+import stampit from "stampit";
 
-// const headers = [
-//   "Category",
-//   "Group",
-//   "Type",
-//   "Hide From Reports",
-//   "Jan 2023",
-//   "Feb 2023",
-//   "Mar 2023",
-//   "Apr 2023",
-//   "May 2023",
-//   "Jun 2023",
-//   "Jul 2023",
-//   "Aug 2023",
-//   "Sep 2023",
-//   "Oct 2023",
-//   "Nov 2023",
-//   "Dec 2023",
-// ];
+const columns = Object.freeze({
+  category: 0,
+  group: 1,
+  typeName: 2,
+  hideFromReports: 3,
+  jan2023: 4,
+  feb2023: 5,
+  mar2023: 6,
+  apr2023: 7,
+  may2023: 8,
+  jun2023: 9,
+  jul2023: 10,
+  aug2023: 11,
+  sep2023: 12,
+  oct2023: 13,
+  nov2023: 14,
+  dec2023: 15,
+});
 
 /**
  * Represents a category that includes properties like name, type, group, and hidden status.
  * This is primarily designed for representing a row from Tiller's Category Table.
  */
-export default class Category {
-  /**
-   * The name of the category.
-   * @type {string}
-   */
-  name = "";
-
-  /**
-   * The type of the category, represented as an instance of CategoryType.
-   * @type {CategoryType}
-   */
-  type = CategoryType.NONE;
-
-  /**
-   * The group that this category belongs to.
-   * @type {string}
-   */
-  group = "";
-
-  /**
-   * Flag to indicate if the category should be hidden from reports.
-   * @type {boolean}
-   */
-  hidden = false;
-  jan2023 = 0;
-  feb2023 = 0;
-  mar2023 = 0;
-  apr2023 = 0;
-  may2023 = 0;
-  jun2023 = 0;
-  jul2023 = 0;
-  aug2023 = 0;
-  sep2023 = 0;
-  oct2023 = 0;
-  nov2023 = 0;
-  dec2023 = 0;
-
-  /**
-   * Creates an instance of the Category class.
-   * @param {Array} row - An array containing cell values for a row from the Category Table.
-   */
-  constructor(row) {
-    const [
-      category,
-      group,
-      type,
-      hideFromReports,
-      jan2023,
-      feb2023,
-      mar2023,
-      apr2023,
-      may2023,
-      jun2023,
-      jul2023,
-      aug2023,
-      sep2023,
-      oct2023,
-      nov2023,
-      dec2023,
-    ] = row;
-
-    this.name = category;
-    this.type = CategoryType.fromString(type);
+const Category = stampit({
+  props: {
+    name: "",
+    type: CategoryType.NONE,
+    group: "",
+    isHidden: false,
+    budget: {
+      jan2023: 0,
+      feb2023: 0,
+      mar2023: 0,
+      apr2023: 0,
+      may2023: 0,
+      jun2023: 0,
+      jul2023: 0,
+      aug2023: 0,
+      sep2023: 0,
+      oct2023: 0,
+      nov2023: 0,
+      dec2023: 0,
+    },
+  },
+  init({ name, type, group, isHidden, budget }) {
+    this.name = name;
+    this.type = type;
     this.group = group;
-    this.hidden = hideFromReports === "Hide";
-    this.jan2023 = jan2023;
-    this.feb2023 = feb2023;
-    this.mar2023 = mar2023;
-    this.apr2023 = apr2023;
-    this.may2023 = may2023;
-    this.jun2023 = jun2023;
-    this.jul2023 = jul2023;
-    this.aug2023 = aug2023;
-    this.sep2023 = sep2023;
-    this.oct2023 = oct2023;
-    this.nov2023 = nov2023;
-    this.dec2023 = dec2023;
-  }
-
-  toArray() {
-    return [
-      this.name,
-      this.group,
-      this.type.toString(),
-      this.hidden ? "Hide" : "",
-      this.jan2023,
-      this.feb2023,
-      this.mar2023,
-      this.apr2023,
-      this.may2023,
-      this.jun2023,
-      this.jul2023,
-      this.aug2023,
-      this.sep2023,
-      this.oct2023,
-      this.nov2023,
-      this.dec2023,
-    ];
-  }
-}
+    this.isHidden = isHidden;
+    this.budget.jan2023 = budget.jan2023;
+    this.budget.feb2023 = budget.feb2023;
+    this.budget.mar2023 = budget.mar2023;
+    this.budget.apr2023 = budget.apr2023;
+    this.budget.may2023 = budget.may2023;
+    this.budget.jun2023 = budget.jun2023;
+    this.budget.jul2023 = budget.jul2023;
+    this.budget.aug2023 = budget.aug2023;
+    this.budget.sep2023 = budget.sep2023;
+    this.budget.oct2023 = budget.oct2023;
+    this.budget.nov2023 = budget.nov2023;
+    this.budget.dec2023 = budget.dec2023;
+  },
+  methods: {
+    fromRow(row) {
+      this.init({
+        name: row[columns.category],
+        type: CategoryType[row[columns.typeName]],
+        group: row[columns.group],
+        isHidden: row[columns.hideFromReports] === "Hidden",
+        budget: {
+          jan2023: row[columns.jan2023],
+          feb2023: row[columns.feb2023],
+          mar2023: row[columns.mar2023],
+          apr2023: row[columns.apr2023],
+          may2023: row[columns.may2023],
+          jun2023: row[columns.jun2023],
+          jul2023: row[columns.jul2023],
+          aug2023: row[columns.aug2023],
+          sep2023: row[columns.sep2023],
+          oct2023: row[columns.oct2023],
+          nov2023: row[columns.nov2023],
+          dec2023: row[columns.dec2023],
+        },
+      });
+    },
+    toRow() {
+      return [
+        this.name,
+        this.group,
+        CategoryType[this.type],
+        this.isHidden ? "Hidden" : "",
+        this.budget.jan2023,
+        this.budget.feb2023,
+        this.budget.mar2023,
+        this.budget.apr2023,
+        this.budget.may2023,
+        this.budget.jun2023,
+        this.budget.jul2023,
+        this.budget.aug2023,
+        this.budget.sep2023,
+        this.budget.oct2023,
+        this.budget.nov2023,
+        this.budget.dec2023,
+      ];
+    },
+  },
+});
