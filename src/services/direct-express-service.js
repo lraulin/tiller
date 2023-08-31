2473ab48ed67fcf0a5ff51ce6246da82ad05a49a
Lee Raulin <leeraulin@gmail.com>
2023-08-31 17:08:23 -0400

    load after to make sure data is fresh

--- src/services/direct-express-service.js
+++ src/services/direct-express-service.js
@@ -28,6 +28,7 @@ const DirectExpressServiceFactory = stampit(BaseSheetServiceFactory, {
       const dupsRemoved = startedWith - this.data.length;
       Logger.log("Removed " + dupsRemoved + " duplicate transactions");
       this.save();
+      this.load();
     },
   }, // #endregion METHODS
 });
