50eae462a975bd6e8824d7a155a64e4d81587c5e
Lee Raulin <leeraulin@gmail.com>
2023-08-31 12:23:59 -0400

    Remove old typedef

--- src/services/category-service.js
+++ src/services/category-service.js
@@ -3,18 +3,6 @@ import CategoryFactory from "../models/category.js";
 import { CategoryService } from "../shared/types.js";
 import stampit from "stampit";
 
-/**
- * CategoryService is responsible for interacting with the 'Categories' sheet.
- * It extends the BaseSheetService to handle category-specific functionality.
- *
- * @typedef {Object} CategoryServiceTypeExtensions
- *
- * @property {Object} lookup - A lookup table for quick category retrieval.
- * @property {Category[]} data - The loaded category data from the Google Sheet.
- *
- * @property {function(string): void} getCategoryData - Retrieves the data for a specified category.
- */
-
 const CategoryServiceFactory = stampit(
   BaseSheetServiceStamp({
     sheetName: "Categories",
