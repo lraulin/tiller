013cc2fcfbfb987126fd1a6bebf91c1054115cf3
Lee Raulin <leeraulin@gmail.com>
2023-08-31 12:38:45 -0400

    fix stampit usage

--- src/services/direct-express-service.js
+++ src/services/direct-express-service.js
@@ -1,28 +1,32 @@
 import BaseSheetService from "./base-sheet-service.js";
 import { DirectExpressService } from "../shared/types.js";
 import DirectExpressTransactionFactory from "../models/direct-express-transaction.js";
+import stampit from "stampit";
 
-const DirectExpressServiceFactory = BaseSheetService({
-  sheetName: "DirectExpress",
-  model: DirectExpressTransactionFactory,
-}).compose({
-  // #region METHODS
-  methods: {
-    getNewTransactions(lastId) {
-      if (lastId === undefined) return [...this.data];
-      return this.data.filter((t) => t.transactionId > lastId);
-    },
+const DirectExpressServiceFactory = stampit(
+  BaseSheetService({
+    sheetName: "DirectExpress",
+    model: DirectExpressTransactionFactory,
+  }),
+  {
+    // #region METHODS
+    methods: {
+      getNewTransactions(lastId) {
+        if (lastId === undefined) return [...this.data];
+        return this.data.filter((t) => t.transactionId > lastId);
+      },
 
-    dedupe() {
-      const lookup = this.data.reduce((acc, c) => {
-        acc[c.transactionId] = c;
-        return acc;
-      }, {});
-      this.data = Object.values(lookup);
-      this.sortByDateDescending();
-    },
-  }, // #endregion METHODS
-});
+      dedupe() {
+        const lookup = this.data.reduce((acc, c) => {
+          acc[c.transactionId] = c;
+          return acc;
+        }, {});
+        this.data = Object.values(lookup);
+        this.sortByDateDescending();
+      },
+    }, // #endregion METHODS
+  }
+);
 
 /** @type {DirectExpressService} */
 const service = DirectExpressServiceFactory();
